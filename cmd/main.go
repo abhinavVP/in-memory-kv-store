f293ffb2d3daee1d25b2ac8073a1422ea1a5c9da
abhinavVP <abhinav.pattanshetti@gmail.com>
2026-04-15 23:02:05 +0530

    smol change to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "kvs/internal/server"
+import (
+	"context"
+	"kvs/internal/server"
+)
 
 func main(){
 	s := &server.Server{}
-	s.Init("12345", "127.0.0.1", 4)
-	s.Listen()
+	s.Init("12345", "127.0.0.1", 4, "")
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
+	s.Listen(ctx, )
 }
